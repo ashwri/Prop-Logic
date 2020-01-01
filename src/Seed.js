f70619c6742c3ef294194857613637001a6d7adc
ashwri <ashley.gail.wright@gmail.com>
2020-01-01 15:37:46 -0800

    Created a seed file which will hold the data for the application

---
+++ src/Seed.js
@@ -0,0 +1,10 @@
+// const Questions = list of steps
+
+
+//Object step = new Object(line, rulesToGetHere, nextPossibleSteps)
+//step representing the starting point will be: {line, null, nextPOssibleSteps}
+//step representing the answer will be: {line, rulesToGetHere, null}
+//string line = ''
+//array nextPossibleSteps = array of steps
+//rulesToGetHere = array or list of rules
+//id: (possibilities: question number and sub step.. this way I can compute the id to use?).. idk yet
