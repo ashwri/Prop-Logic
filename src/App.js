0446c31de79676028e367d531c2b2df424c4ffc3
ashwri <ashley.gail.wright@gmail.com>
2020-01-03 16:05:41 -0800

    Converted the App container from stateful to stateless component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Question from './Question/Question';
 import Seed from './Seed.js';
 
-//TODO: This component will be stateless.
-//TODO Phase 2: Figure out a key for the data when it is being mapped
-class App extends Component {
+const questions = Seed.questions;
 
-  questions = Seed.questions;
+const App = () => {
 
-  render() {
-    const questionComponents = this.questions.map((question) => (
-      <Question
-        text={question.line}
-        nextSteps={question.nextPossibleSteps}
-        key={question.id}
-        id={question.id}
-      />
-    ));
-    return (
-      <div className="App">
-        {questionComponents}
-      </div>
-    );
-  }
+  const questionComponents = questions.map((question) => (
+    <Question
+      text={question.line}
+      nextSteps={question.nextPossibleSteps}
+      key={question.id}
+      id={question.id}
+    />
+  ));
+  return (
+    <div className="App">
+      {questionComponents}
+    </div>
+  );
 }
+
 
 export default App;
