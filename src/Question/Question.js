de8d432cff29bf0d57ed4d8d30e5a4f47a9ff6ec
ashwri <ashley.gail.wright@gmail.com>
2020-02-02 11:00:31 -0800

    Back Button Working

--- src/Question/Question.js
+++ src/Question/Question.js
@@ -8,10 +8,23 @@ import '../Question/Question.css';
 //TODO Phase X: Figure out a better way to implement that chunk of code.
 
 class Question extends Component {
+    //stack here..
+    //add the state to the stack
+    //add the state of createLine to the stack.. maybe I will have to create different variables to hold this value?
+    //{nextSteps, text, lineText} //probably don't need the text since that will probably be in the lineText
+    stateStack = [];
+
 
     state = {
         nextSteps: this.props.nextSteps,
         text: this.props.text
+    };
+
+
+    stackPush = () => {
+        let currState = this.state;
+        let text = this.lines;
+        this.stateStack.push({ currState, text });
     }
 
     lines = [this.props.text];
@@ -53,6 +66,69 @@ class Question extends Component {
         }
     }
 
+    //should this method actually give all the possible next steps instead of just choosing the first one?
+    nextStepHandler = (event) => {
+        if (this.state.nextSteps != null) { //if(this.state.nextSteps)
+            this.updateLines(0);
+            this.updateState(0);
+        }
+    }
+
+    nextPossibleStepsHandler = (event) => {
+        this.highlightPossibleSteps();
+    }
+
+    highlightPossibleSteps = () => {
+        for (let i = 0; i < this.state.nextSteps.length; i++) {
+            //TODO: highlight rule to be applied to get there
+        }
+    }
+
+    backButtonHandler = (event) => {
+        console.log("I am popping...", this.stateStack.pop());
+
+        let info = this.stateStack.pop();
+        console.log("I am also popping (new state)", info);
+        // console.log(info);
+        let newState = {
+            nextSteps: info.currState.nextSteps,
+            text: info.currState.text
+        }
+        this.setPrevState(newState);
+        this.lines = info.text;
+        //remove from stack
+        //set the state to the values removed from the stack
+        //set the lineText to the value from the stack
+    }
+
+    setPrevState = (newState) => {
+        this.setState((props) => {
+            return {
+                nextSteps: newState.nextSteps,
+                text: newState.text
+            };
+
+        });
+    }
+
+
+
+    possibleSolutionsHandler = (event) => {
+        //get value at the bottom of the stack (original question and state)
+        //
+    }
+
+    printPossibleSolutions = (info) => {
+        //this will require some dynamic programming
+        console.log(info);
+        for (let i = 0; i < this.state.nextSteps.length; i++) {
+            this.printPossibleSolutions(i);
+        }
+    }
+
+    startOverHandler = (event) => {
+        //set the items to the bottom of the stack;
+    }
 
     createLine = (lineText) => {
         return <Line text={lineText} key={this.props.id + lineText} />
@@ -65,6 +141,9 @@ class Question extends Component {
 
 
     render() {
+        this.stackPush();
+        console.log("The stack: ", this.stateStack);
+        console.log("The current state: ", this.state);
         return (
             <div>
                 <div className="grid-container">
@@ -76,12 +155,14 @@ class Question extends Component {
                 <div>
                     <button> Start Over Icon </button>
                     <button> Next Step </button>
+                    <button onClick={this.backButtonHandler}> Last Step </button>
                     <button> Possible Soultions </button>
                 </div>
 
-            </div>
+            </div >
 
         )
+
     }
 
 
